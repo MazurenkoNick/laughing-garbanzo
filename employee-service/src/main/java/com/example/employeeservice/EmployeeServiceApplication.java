0a33d80f1ff1ee42d8aabe71cce97dcf3cefad95
nikita <nikitamazurenko42@gmail.com>
2023-03-25 15:06:02 +0200

    implementing api-gateway

--- employee-service/src/main/java/com/example/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/example/employeeservice/EmployeeServiceApplication.java
@@ -2,13 +2,17 @@ package com.example.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class EmployeeServiceApplication {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
